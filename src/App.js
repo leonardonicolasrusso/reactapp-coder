e9497366e0a25c65afd8da1731f782e9be6424af
szuviria <szuviria@gmail.com>
2021-09-08 22:04:07 -0300

    consumiendo apis

--- src/App.js
+++ src/App.js
@@ -1,64 +1,40 @@
-import { useState } from 'react'
-// import logo from './logo.svg';
+import { useState, 
+  // useEffect 
+} from 'react'
 import './App.css';
-import NavBar from './components/NavBar/NavBar.js'
-import Counter from './components/Counter/Counter.js'
+
+
 
 const App = () => {
-  
-const [contadores, setContadores] = useState({
-  contador1: 0,
-  contador2: 0,
-  contador3: 0
-})
+  const [productos, setProductos] = useState([])
+  const [input, setInput] = useState('')
 
-const funBoton1 = () => {
-  const newContador = {
-    ...contadores,
-    contador1: contadores.contador1 + 1,
+  // useEffect(() => {
+  //   fetch('https://api.mercadolibre.com/sites/MLA/search?q=iphone')
+  //   .then( res => { return res.json()}).then( function (respuesta) {
+  //     setProductos(respuesta.results.slice(0, 10))
+  //   })
+  // }, [])
+
+
+  const handleClick = () => {
+    fetch(`https://api.mercadolibre.com/sites/MLA/search?q=${input}`)
+    .then( res => { return res.json()}).then( function (respuesta) {
+      setProductos(respuesta.results.slice(0, 10))
+    })
   }
-  setContadores(newContador)
-}
-
-const funBoton2 = () => {
-  const newContador = {
-    ...contadores,
-    contador2: contadores.contador2 + 1,
-  }
-  setContadores(newContador)
-}
-const funBoton3 = () => {
-  const newContador = {
-    ...contadores,
-    contador3: contadores.contador3 + 1,
-  }
-  setContadores(newContador)
-}
-
-//  const miFuncionRestar = () => {
-//      if(count > 0) {
-//        setCount(count - 1)
-//      }
-
 
   return (
     <div className="App">
-      <header>
-        <NavBar /> 
-      </header>
-      <Counter inicial="" max="25"/>
-      <h1>{contadores.contador1}</h1>
-      <h1>{contadores.contador2}</h1>
-      <h1>{contadores.contador3}</h1>
-      <button onClick={funBoton1}>boton1</button>
-      <button onClick={funBoton2}>boton2</button>
-      <button onClick={funBoton3}>boton2</button>
-      {/* <h1 style={ {color: 'white'}}>{count}</h1>
-      <button onClick={() => setView(!view)}>Mostrar Navbar o no</button>
-      <button onClick={() => setCount(count + 1)}>Sumar al contador</button>
-      <button onClick={miFuncionRestar}>Restar al contador</button>
-      <img src={logo} className="App-logo" alt="logo" />
-      { count <= 0 ? <h1 style={{ color: 'white'}}>EL CONTADOR YA ES CERO</h1> : <h1 style={{ color: 'white'}}>EL CONTADOR NO ES CERO</h1>} */}
+      <div>
+        <input type="text" onChange={(event) => setInput(event.target.value)}/>
+        <button onClick={handleClick}>Buscar</button>
+      </div>
+      <ul>
+        { productos.map(prod => {
+          return <li style={{ color: 'white' }} key={prod.id}>{prod.title}</li>
+        })}
+      </ul>
     </div>
   );
 }
