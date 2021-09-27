3422796fb2327253439b807797d749d003a09ad2
szuviria <szuviria@gmail.com>
2021-09-27 00:15:07 -0300

    firebase

--- src/services/firebase/firebase.js
+++ src/services/firebase/firebase.js
@@ -2,12 +2,12 @@ import * as firebase from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBWsmFK0XiQsg9YtrGsmP4-VIEVm7WFn5I",
-    authDomain: "mi-aplicacion-6511f.firebaseapp.com",
-    projectId: "mi-aplicacion-6511f",
-    storageBucket: "mi-aplicacion-6511f.appspot.com",
-    messagingSenderId: "107062922167",
-    appId: "1:107062922167:web:f897f11b1cbbd255a07632"
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId
 }
 
 const app = firebase.initializeApp(firebaseConfig)
